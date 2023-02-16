caf3202e179ac6d461705743b0a5096a5b17c8a3
podowd <podowd@users.noreply.github.com>
2023-02-16 15:34:03 +0000

    basic expressjs web app

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(`Endpoint called using HTTP GET`)
+  res.send('Hello World 123!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example server listening on port ${port}`)
 })
